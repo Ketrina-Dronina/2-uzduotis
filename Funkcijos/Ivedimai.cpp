3b2c87585f6f7ec4c28085f61fdadb9e87eb62c8
Ketrina-Dronina <91048600+Ketrina-Dronina@users.noreply.github.com>
2021-12-03 17:38:45 +0200

    Add files via upload

---
+++ Funkcijos/Ivedimai.cpp
@@ -0,0 +1,244 @@
+#include "Studentas.h"
+#include "Studentas.cpp"
+
+void fileIvedimas(vector<Studentas> &studentai, string file)
+{
+	string eil;
+	int pazymys;
+	stringstream my_buffer;
+	ifstream openf;
+	openf.open(file + ".txt");
+	while (!openf)
+	{
+		cout << "Nerastas " << file << ".txt failas!" << endl;
+		cin >> file;
+		openf.open(file + ".txt");
+	}
+	my_buffer << openf.rdbuf();
+	openf.close();
+	auto start = std::chrono::high_resolution_clock::now();
+	auto st = start;
+	getline(my_buffer, eil);
+	string vardas, pavarde;
+	int j = 0;
+	while (getline(my_buffer, eil))
+	{
+		j++;
+		istringstream is(eil);
+		is >> vardas >> pavarde;
+		Studentas studentas(vardas, pavarde);
+		while (is >> pazymys)
+		{
+			if (filetikrinimas(pazymys))
+			{
+				studentas.setPazymiai(pazymys);
+			}
+			else
+				break;
+		}
+		if (filetikrinimas(pazymys))
+		{
+			studentas.pasalintiPaskutiniPaz();
+			studentas.egzaminoPaz(pazymys);
+			studentas.setVidurkis();
+			studentas.setMediana();
+			studentai.push_back(studentas);
+		}
+		else
+		{
+			cout << j << "-oje eiluteje yra klaida!" << endl;
+		}
+	}
+	std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now() - start; // Skirtumas (s)
+	std::cout << file << " failo nuskaitymas uztruko: " << diff.count() << " s\n";
+}
+
+void Ivedimas(vector<Studentas> &studentai)
+{
+	int size;
+	int pazymys;
+	string vardas, pavarde;
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::mt19937 generator(seed);
+	std::uniform_int_distribution<int> distribution(1, 10);
+	cout << "Iveskite varda: ";
+	cin >> vardas;
+	cout << "Iveskite pavarde: ";
+	cin >> pavarde;
+	Studentas studentas(vardas, pavarde);
+	cout << "Ar zinote namu darbu skaiciu? (t-taip, n-ne)" << endl;
+	string ndskz = Rtikrinimas();
+	if (ndskz == "t")
+	{
+		cout << "Iveskite namu darbu skaiciu: ";
+		cin >> size;
+		do
+		{
+			try
+			{
+				if (size < 1)
+					throw std::invalid_argument("\n---Pakartokite ivedima, pastebimas netinkamas simbolis\n");
+			}
+			catch (const std::invalid_argument &e)
+			{
+				cout << e.what();
+				cin.clear();
+				cout << "Iveskite namu darbu skaiciu: ";
+				cin >> size;
+			}
+		} while (size < 1);
+
+		cout << "Ar norite namu darbus ivesti ranka? (t-taip, n-ne): ";
+		string ndskr = Rtikrinimas();
+		if (ndskr == "t" || ndskr == "T")
+		{
+			cout << "Iveskite pazymius." << endl;
+			for (int j = 0; j < size; j++)
+			{
+				cout << j + 1 << "-asis namu darbas: ";
+				pazymys = Sktikrinimas();
+				"\n";
+				studentas.setPazymiai(pazymys);
+			}
+			cout << "Iveskite egzamino rezultata: ";
+			studentas.egzaminoPaz(Sktikrinimas());
+		}
+		else
+		{
+			cout << "Atsitiktinai sugeneruoti namu darbai: ";
+			for (int j = 0; j < size; j++)
+			{
+				int a = distribution(generator);
+				studentas.setPazymiai(a);
+				cout << a << " ";
+			}
+			studentas.egzaminoPaz(distribution(generator));
+			cout << "\nAtsitiktinai sugeneruotas egzamino rezultatas: " << studentas.getEgzaminas() << endl;
+		}
+		studentas.setVidurkis();
+		studentas.setMediana();
+	}
+	else
+	{
+		int pazymys;
+		size = 0;
+		cout << "--Noredami nutraukti ivedima iveskite 0!" << endl;
+		cout << size + 1 << "-asis namu darbas" << endl;
+		studentas.setPazymiai(Sktikrinimas());
+		while (true)
+		{
+			size++;
+			cout << size + 1 << "-asis namu darbas: ";
+			pazymys = Paztikrinimas();
+			if (pazymys == 0)
+			{
+				break;
+			}
+			else
+			{
+				studentas.setPazymiai(pazymys);
+			}
+		}
+		size--;
+		cout << "Iveskite egzamino rezultata: ";
+		studentas.egzaminoPaz(Sktikrinimas());
+	}
+	studentai.push_back(studentas);
+	cout << "Ar norite ivesti dar viena studenta? (t-taip, n-ne)" << endl;
+}
+
+void RandomIvedimas(int n)
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	auto st = start;
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::mt19937 generator(seed);
+	std::uniform_int_distribution<int> distribution(1, 10);
+	std::uniform_int_distribution<int> FGdistribution(5, 15);
+	string studentaiFile = "studentai" + std::to_string(n);
+	ofstream fd(studentaiFile + ".txt");
+	fd << left << setw(15) << "Vardas" << left << setw(20) << "Pavarde";
+	int GradeCount = FGdistribution(generator);
+	for (int i = 0; i < GradeCount; i++)
+	{
+		fd << left << setw(15) << "ND" + std::to_string(i + 1);
+	}
+	fd << left << setw(15) << "Egz." << endl;
+	for (int i = 0; i < n; i++)
+	{
+		fd << left << setw(15) << "Vardas" + std::to_string(i + 1) << left << setw(20) << "Pavarde" + std::to_string(i + 1);
+		for (int j = 0; j < GradeCount; j++)
+		{
+			fd << left << setw(15) << distribution(generator);
+		}
+		fd << left << setw(15) << distribution(generator) << endl;
+	}
+	fd.close();
+	std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now() - start; // Skirtumas (s)
+	std::cout << "studentai_" << n << " failo sukurimas uztruko: " << diff.count() << " s\n";
+}
+
+template <class Z>
+void BenchmarkIvedimas(Z &studentai, string file)
+{
+	string eil, vardas, pavarde;
+	int pazymys;
+	stringstream my_buffer;
+	ifstream openf;
+	try
+	{
+		openf.open(file + ".txt");
+		if (!openf)
+		{
+			throw file;
+		}
+
+		my_buffer << openf.rdbuf();
+		openf.close();
+
+		auto start = std::chrono::high_resolution_clock::now();
+		auto st = start;
+		getline(my_buffer, eil);
+		int j = 0;
+		while (getline(my_buffer, eil))
+		{
+			j++;
+			istringstream is(eil);
+			is >> vardas >> pavarde;
+			Studentas studentas(vardas, pavarde);
+			while (is >> pazymys)
+			{
+				if (filetikrinimas(pazymys))
+				{
+					studentas.setPazymiai(pazymys);
+				}
+				else
+					break;
+			}
+			if (filetikrinimas(pazymys))
+			{
+				studentas.pasalintiPaskutiniPaz();
+				studentas.egzaminoPaz(pazymys);
+				studentas.setVidurkis();
+				studentas.setMediana();
+				studentai.push_back(studentas);
+			}
+			else
+			{
+				throw j;
+			}
+		}
+		std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now() - start; // Skirtumas (s)
+		std::cout << file << " failo nuskaitymas uztruko: " << diff.count() << " s\n";
+	}
+	catch (string file)
+	{
+		cout << "Neegzistuoja " << file << ".txt failas. Duomenu spartos analize nutraukiama" << endl;
+		exit(0);
+	}
+	catch (int j)
+	{
+		cout << "Faile " << file << ".txt " << j + 1 << "-eiluteje yra klaida. Duomenu spartos analize nutraukiama" << endl;
+		exit(0);
+	}
+}
